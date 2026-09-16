413b764bd7f7a7e8542774b2b038ed2401a92029
David Seto <67933197+dtseto@users.noreply.github.com>
2026-09-15 21:53:56 -0700

    Add isolated ARM64 opcode test mode

--- BasiliskII/src/uae_cpu_2021/basilisk_glue.cpp
+++ BasiliskII/src/uae_cpu_2021/basilisk_glue.cpp
@@ -32,6 +32,10 @@
 #include "newcpu.h"
 #include "compiler/compemu.h"
 
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 // RAM and ROM pointers
 uint32 RAMBaseMac = 0;		// RAM base (Mac address space) gb-- initializer is important
@@ -140,9 +144,126 @@ void InitFrameBufferMapping(void)
  *  Reset and start 680x0 emulation (doesn't return)
  */
 
+static bool run_opcode_test_mode()
+{
+    const char *hex = getenv("B2_TEST_HEX");
+    if (!(hex && *hex))
+        return false;
+
+    uint16 words[1024];
+    size_t word_count = 0;
+    const char *cursor = hex;
+    while (*cursor) {
+        while (*cursor && (isspace((unsigned char)*cursor) || *cursor == ',' || *cursor == ';' || *cursor == ':'))
+            cursor++;
+        if (!*cursor)
+            break;
+        if (word_count >= lengthof(words)) {
+            fprintf(stderr, "B2_TEST_HEX has too many words\\n");
+            quit_program = 1;
+            return true;
+        }
+        char *end = NULL;
+        unsigned long value = strtoul(cursor, &end, 16);
+        if (end == cursor || value > 0xffff) {
+            fprintf(stderr, "B2_TEST_HEX parse failed\\n");
+            quit_program = 1;
+            return true;
+        }
+        words[word_count++] = (uint16)value;
+        cursor = end;
+    }
+    if (word_count == 0) {
+        fprintf(stderr, "B2_TEST_HEX is empty\\n");
+        quit_program = 1;
+        return true;
+    }
+
+    const uaecptr test_addr = RAMBaseMac + 0x1000;
+    const uaecptr stack_addr = RAMBaseMac + RAMSize - 0x2000;
+    for (size_t i = 0; i < word_count; i++)
+        put_word(test_addr + (uaecptr)(i * 2), words[i]);
+    put_word(test_addr + (uaecptr)(word_count * 2), M68K_EXEC_RETURN);
+
+    for (int i = 0; i < 8; i++) {
+        m68k_dreg(regs, i) = 0;
+        m68k_areg(regs, i) = 0;
+    }
+    m68k_areg(regs, 7) = stack_addr;
+    regs.usp = regs.isp = regs.msp = stack_addr;
+    regs.sr = 0x2700;
+
+    const char *init = getenv("B2_TEST_INIT");
+    if (init && *init) {
+        uint32 init_values[17];
+        size_t init_count = 0;
+        cursor = init;
+        while (*cursor) {
+            while (*cursor && (isspace((unsigned char)*cursor) || *cursor == ',' || *cursor == ';' || *cursor == ':'))
+                cursor++;
+            if (!*cursor)
+                break;
+            if (init_count >= lengthof(init_values))
+                break;
+            char *end = NULL;
+            unsigned long value = strtoul(cursor, &end, 16);
+            if (end == cursor || value > 0xffffffffUL) {
+                fprintf(stderr, "B2_TEST_INIT parse failed\\n");
+                quit_program = 1;
+                return true;
+            }
+            init_values[init_count++] = (uint32)value;
+            cursor = end;
+        }
+        if (init_count != 16 && init_count != 17) {
+            fprintf(stderr, "B2_TEST_INIT needs 16 or 17 values\\n");
+            quit_program = 1;
+            return true;
+        }
+        for (int i = 0; i < 8; i++)
+            m68k_dreg(regs, i) = init_values[i];
+        for (int i = 0; i < 8; i++)
+            m68k_areg(regs, i) = init_values[8 + i];
+        if (init_count == 17)
+            regs.sr = (uint16)(init_values[16] & 0xffff);
+        regs.usp = regs.isp = regs.msp = m68k_areg(regs, 7);
+    }
+
+    MakeFromSR();
+    regs.stopped = 0;
+    SPCFLAGS_CLEAR(SPCFLAG_STOP | SPCFLAG_BRK | SPCFLAG_DOTRACE | SPCFLAG_TRACE);
+    m68k_setpc(test_addr);
+    fill_prefetch_0();
+    quit_program = 0;
+#if USE_JIT
+    if (UseJIT)
+        m68k_compile_execute();
+    else
+#endif
+        m68k_execute();
+
+    MakeSR();
+    fprintf(stderr,
+        "REGDUMP: D0=%08x D1=%08x D2=%08x D3=%08x D4=%08x D5=%08x D6=%08x D7=%08x "
+        "A0=%08x A1=%08x A2=%08x A3=%08x A4=%08x A5=%08x A6=%08x A7=%08x SR=%04x\\n",
+        (unsigned)m68k_dreg(regs, 0), (unsigned)m68k_dreg(regs, 1),
+        (unsigned)m68k_dreg(regs, 2), (unsigned)m68k_dreg(regs, 3),
+        (unsigned)m68k_dreg(regs, 4), (unsigned)m68k_dreg(regs, 5),
+        (unsigned)m68k_dreg(regs, 6), (unsigned)m68k_dreg(regs, 7),
+        (unsigned)m68k_areg(regs, 0), (unsigned)m68k_areg(regs, 1),
+        (unsigned)m68k_areg(regs, 2), (unsigned)m68k_areg(regs, 3),
+        (unsigned)m68k_areg(regs, 4), (unsigned)m68k_areg(regs, 5),
+        (unsigned)m68k_areg(regs, 6), (unsigned)m68k_areg(regs, 7),
+        (unsigned)regs.sr);
+    quit_program = 1;
+    return true;
+}
+
 void Start680x0(void)
 {
 	m68k_reset();
+	if (run_opcode_test_mode())
+		return;
 #if USE_JIT
     if (UseJIT)
 	m68k_compile_execute();
