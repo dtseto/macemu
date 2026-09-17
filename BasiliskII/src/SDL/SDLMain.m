fa32e8daa7b71c62654e3c369779e374dd1833c8
David Seto <67933197+dtseto@users.noreply.github.com>
2026-09-17 16:15:48 -0700

    sdl3
    sdl3 updates

--- BasiliskII/src/SDL/SDLMain.m
+++ BasiliskII/src/SDL/SDLMain.m
@@ -5,10 +5,10 @@
     Feel free to customize this file to suit your needs
 */
 
-#include "SDL.h"
+#include "my_sdl.h"
 #if SDL_VERSION_ATLEAST(3, 0, 0)
 #define SDL_MAIN_NEEDED
-#include <SDL_main.h>
+#include <SDL3/SDL_main.h>
 #undef SDL_QUIT
 #define SDL_QUIT	SDL_EVENT_QUIT
 #else
